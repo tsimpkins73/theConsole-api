ce5db8028432c307f51d25d9e090aab32d867308
tsimpkins73 <46323925+tsimpkins73@users.noreply.github.com>
2019-11-12 12:09:28 -0500

    API Hookup edits

--- seeds/seed.console_articles.sql
+++ seeds/seed.console_articles.sql
@@ -507,3 +507,102 @@ Useful links and resources
 Libraries for building surveys: SurveyJS
 
 Some commercial survey services include: Survey Monkey and Typeform', 'https://cdn-media-1.freecodecamp.org/images/0*v3qXmKe1LTiiW_3H.png', '[Tutorials]', '[]', 'false'),
+('https://www.smashingmagazine.com/2019/11/exploring-new-ways-manage-content-wordpress/', 'Exploring New Ways To Manage Content In WordPress', 'The combination of WordPress"’" versatility for managing data (since its database model supports the creation of different content models, easily extensible through meta attributes) and Gutenberg"’"s rich user interactions provide a powerful mechanism to create, edit and manage content.
+
+In this article, I want to shine some light on these upgraded capabilities, exploring the new tools at our disposition and presenting several new ones to be released sometime in the future.', 'The combination of WordPress"’" versatility for managing data (since its database model supports the creation of different content models, easily extensible through meta attributes) and Gutenberg"’"s rich user interactions provide a powerful mechanism to create, edit and manage content.
+
+In this article, I want to shine some light on these upgraded capabilities, exploring the new tools at our disposition and presenting several new ones to be released sometime in the future.
+
+Existing Features
+The following features are already part of Gutenberg-powered WordPress.
+
+CREATE ONCE, PUBLISH EVERYWHERE
+As I have described in my recent article “Create Once, Publish Everywhere” with WordPress, the block-based nature of Gutenberg enables it to enhance how content is organized/architected on the database, making it available on a granular basis (block by block) to any application running on any medium or platform (web, email, iOS/Android apps, VR/AR, home assistants, and so on). Content managed through Gutenberg can then become the single source of truth for all of our applications, allowing us to reduce the cost associated with re-formatting content to make it suitable for each required platform.
+
+COPY/PASTE FROM GOOGLE DOCS WITH (ALMOST) PERFECT FORMATTING
+Whenever we need to collaborate with other people to create content, we will quite likely use online tools such as Google Docs, Dropbox Paper, Coda or others. These tools make it easy for different people to edit the content in a document concurrently and provide and incorporate feedback. If we are going to choose a Content Management System to store our content, we need to make sure that it works well with these tools.
+
+The web is full of interfaces that leave people out. We can fix it. Meet Inclusive Components, our new practical book for building accessible and robust interfaces. From accordions, tables and tabs to buttons, notifications and modals — all in one book. Written by Heydon Pickering.
+
+Check table of contents ↬
+Feature Panel
+Gutenberg does the job fairly well: When copying the content from a Google Doc and then pasting it into a Gutenberg blog post, the formatting is preserved, bullet lists are properly transformed to the list block, and images are inserted where they should. There may be a few inconsistencies (for instance different spacing across blocks and in the original document) however, for the most part, the process is fit for use.
+
+Copy/pasting from GDoc to Gutenberg
+Copy/pasting from GDoc preserves the format of the document. (Large preview)
+CRAFTING ART DIRECTION
+Several Gutenberg blocks support creating distinctive and engaging layouts and assisting the art direction of the site, to give it more personality and emphasize its identity. This way, even though we may base the site on a standard, plain-looking WordPress theme, we can customize the content’s appearance to make it stick out from the sea of sameness out there on the web. Let’s explore some of these blocks.
+
+The Shape divider block allows to insert dividers in between two blocks. We can choose one among several basic shapes and customize its width, proportions and colors and then, with a bit of resourcefulness, create more intrinsic patterns from it. For instance, the divider below was created by first creating and customizing a divider, then flipping it both vertically and horizontally to mirror itself, then grouping these 2 halves so we can use it as a single unit (the grouping functionality will be available in core through the release of WordPress 5.3 next week, and is currently available through the Gutenberg plugin), and finally saving the grouped block as a reusable block so it can be used everywhere across the whole site:
+
+Shape divider block
+The shape divider block connects, or breaks apart, components. (Large preview)
+The Advanced columns and Row layout blocks allow to create row-based layouts, inside of which we can place nested blocks (i.e. any other Gutenberg block). They are highly configurable: They offer to define how many columns the row must have, with what padding, margin and proportion of width for each column, setting an image or custom color in the background, and several other attributes.
+
+Row layout block
+The row layout block allows to easily configure the proportion of width among columns. (Large preview)
+We can also create grid-based layouts with predefined content. For instance, through the Post grid, Post carousel and Post masonry blocks we can display a list of posts in different ways, defining what attributes from each post to show (title, date, excerpt, author, and so on), and through the Advanced gallery block we can create beautiful image galleries.
+
+Advanced gallery block
+Masonry gallery created with Advanced gallery block. (Large preview)
+Some other blocks, such as Feature grid, allow to create grid layouts with predefined templates filled with custom content.
+
+Feature grid block
+The feature grid block enables to add custom content inside of a grid layout. (Large preview)
+These are just a sample of those blocks which can help us fill the content with visually attractive layouts and craft the art direction of our sites. To keep exploring possibilities, we can head to the directory of plugins offering blocks and check these out.
+
+ASSISTING THE USER WHILE EDITING CONTENT
+Gutenberg assists the user when creating content through the following features:
+
+Real-Time Preview
+The Gutenberg editor gives a relatively accurate preview of how the content will look like in the website.
+
+Error Warnings
+Gutenberg makes the content creator be aware of accessibility concerns. For instance, if our content structure jumps from an <h2> header to an <h4> one without adding an <h3> tag in between, Gutenberg provides a warning message about this potential error. Similarly, when setting up the color of some text against its background color, if the contrast between the two colors is not clear enough then Gutenberg provides a warning message and helps fix the problem.
+
+Suggesting/Executing Improvements
+Blocks can connect to third-party services to analyze content and enhance it. For instance, a service could suggest how to improve the grammar of the content, provide alternative titles and tags for a better SEO, and even automatically translate the content to another language, as done by this plugin which automatically translates from English to Hindi as the user types.
+
+New Features Under Implementation
+The following features will hopefully/eventually be coming to Gutenberg in the future.
+
+SNAP TO GRID WHEN RESIZING IMAGES
+Contributors are already working on adding a grid system to Gutenberg which will, among other things, enable to resize images in an assisted manner by snapping it to the grid:
+
+Installing a block from within Gutenberg
+Snapping an image to grid (image from the GitHub issue). (Large preview)
+INLINE INSTALLATION OF BLOCKS
+Sometimes, while we are writing a blog post, we find out that we need some functionality that we don’t have yet installed. Hence, we need to switch to the Plugins screen, search and install the corresponding plugin, and then go back to the blog post. This process adds friction to our content-writing workflow.
+
+Wouldn’t it be nicer if we could install the required functionality right from within the editor itself, whenever we need to use it? Well, this proposal is already being implemented through this pull request (it first depends on blocks being installed on their own, i.e. without depending on being shipped through a plugin). Once merged, our content-writing workflow will not be impaired anymore, as visible in the mockup below.
+
+Installing a block from within Gutenberg
+Installing a block from within Gutenberg (image from the GitHub pull request). (Large preview)
+Installing blocks directly from the editor could lead to unintended bloat, from making it too easy for the user to install blocks. To address this issue, after installing and using it, the block could be removed! This was not possible before Gutenberg, because if the plugin providing a shortcode (which was the way to render dynamic content inside the blog post before Gutenberg) was disabled, then the invocation of the shortcode would be rendered in the blog post (instead of the shortcode’s output), messing up our content. However, Gutenberg works differently: Blocks only save HTML content inside of the blog post (including HTML comments to store configuration attributes), so, if the block is disabled, its intended HTML output is still part of the blog post’s content. (Even though there may be problems if the block needs to load CSS assets which are not loaded anymore once the block is disabled. I am not aware how this issue will be handled.)
+
+PAGE/SITE BUILDER
+Currently Gutenberg can only be used for the creation of content inside a blog post or a page, however it will soon support the creation of any part of the website: Content-block areas can define the header, sidebars, footer or any section needed for our layouts. Automattic (the company behind WordPress.com) is already working on a plugin to add full site editing capabilities to its WordPress.com product, which should eventually be extensible to the open source WordPress software too.
+
+Creating a new page with full site editing enabled
+Creating a new page with full site editing allows to select a page template. (Large preview)
+REAL-TIME COLLABORATION
+Google Docs is incredibly useful to teams because it enables their members to work on the same document at the same time. Sometime in the future, Gutenberg will also incorporate a mechanism for real-time collaboration, allowing different people to work on the same blog post at the same time. This mechanism will (at least initially) be based on giving editing-locks to users on a block-by-block basis, as shown in the mockup image below.
+
+Real-time collaboration through Gutenberg
+Real-time collaboration through Gutenberg (image from the GitHub issue). (Large preview)
+This feature will be particularly useful to online magazines (such as the New York Times and the like) since they may already have teams collaborating on a story (for instance, designers dealing with images, journalists, proofreaders and editors dealing with content, and others). Having real-time collaboration tools will enable these magazines to speed up their content-creation workflows and publish their articles faster.
+
+TRANSLATION
+WordPress core has never added support to translate content (it only supports translation of strings inside of core, plugin and theme files), but instead left this responsibility to plugins. Through Gutenberg, WordPress will finally add native support for this feature.
+
+Translation is not a priority yet, so it has been targeted for Gutenberg phase 4, expected in the year 2020+. Since it is a long way off, there are yet no technical considerations of its implementation or mockups of its intended user experience. So we can only guess how it will be. Since it will be implemented after the real-time collaboration feature (described above), I would expect it will enable different people to translate the same blog post to different languages at the same time, block by block.
+
+INLINE MEDIA EDITING
+Through the Media Library, WordPress already provides some image editing capabilities: resizing, cropping, rotating and flipping. These capabilities are very basic, and they are applied on to the image on a different screen, which creates some friction to the process of fitting the image into the blog post.
+
+Through Gutenberg, the media-editing experience could be greatly enhanced: One one side, it could support editing the image in more advanced ways, such as applying effects or filters, altering the contrast, replacing colors, adding text as watermark, adding transparent regions, converting it to different formats, and others (for instance, Cloudinary provides an API to apply many transformations to an image, which could be perfectly accessed by a block). On the other side, the editing could happen inline, right where the image is placed inside the blog post. Then, for instance, if the image was added as an overlay against some background, and we add transparent regions to the image, we can visualize in real-time how the composite result looks like.
+
+(I haven’t found any proposal to tackle this issue in Gutenberg’s GitHub repo, but I learned about this idea talking to some core contributors, who expected to be able to work on it some time in the future.)
+
+Conclusion
+Already being the most popular CMS (close to 35% of the web), WordPress has also the chance to offer the most compelling tools to manipulate content. This is because Gutenberg offers an appealing mechanism to create, edit and manage content: A single interface, simple to use, fairly powerful and versatile. With its new content management capabilities, WordPress can become the single source of truth of all our content, to power all our applications (websites, newsletters, apps, and so on) through APIs. Kudos to that!', 'https://res.cloudinary.com/indysigner/image/fetch/f_auto,q_auto/w_1600/https://cloud.netlifyusercontent.com/assets/344dbf88-fdf9-42bb-adb4-46f01eedd629/95cc313a-b4d1-49a7-aebf-6f636ba4101c/advanced-gallery-block.jpg', '[Tips, Extended Learning]', '[]', 'true'),
