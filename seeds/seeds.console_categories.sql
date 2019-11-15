d6cb61b3181d6b6d6fd50aadfe6fb9085b674449
tsimpkins73 <46323925+tsimpkins73@users.noreply.github.com>
2019-11-14 23:10:32 -0500

    Database Edits

---
+++ seeds/seeds.console_categories.sql
@@ -0,0 +1,3 @@
+INSERT INTO categories (name, article_id)
+VALUES
+()
---
+++ seeds/seeds.console_users.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (username, password, name)
+VALUES
+('GenBlank', 'pass', 'General Blank'),
+('janeDoe', 'pass', 'Jane Doe');
