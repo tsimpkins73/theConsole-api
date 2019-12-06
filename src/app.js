001f7b455e9f13f33cfe0ae5e81a72297254ccff
Travis Simpkins <t73designs@yahoo.com>
2019-12-06 11:27:40 -0500

    Client Hookup

--- src/app.js
+++ src/app.js
@@ -1,49 +1,39 @@
-require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
-const { NODE_ENV } = require('./config.js')
-const app = express()
-const article_router = require('./articles/article-router.js')
+const { NODE_ENV, CLIENT_ORIGIN } = require('../config')
+const articlesRouter = require('./articles/articles-router')
+const commentsRouter = require('./comments/comments-router')
 const authRouter = require('./auth/auth-router')
 const usersRouter = require('./users/users-router')
-const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
 
-app.use(morgan(morganSetting))
+const app = express()
+
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+  skip: () => NODE_ENV === 'test',
+}))
+app.use(
+  cors({
+      origin: CLIENT_ORIGIN
+  })
+);
 app.use(helmet())
-app.use(cors())
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
 
-/* app.use(validator)
-app.use(errorHandler)
- */
-
- app.use(article_router)
- app.use('/api/auth', authRouter)
+app.use('/api/articles', articlesRouter)
+app.use('/api/comments', commentsRouter)
+app.use('/api/auth', authRouter)
 app.use('/api/users', usersRouter)
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!')
-})
 app.use(function errorHandler(error, req, res, next) {
   let response
   if (NODE_ENV === 'production') {
-    response = {
-      error: {
-        message: 'server error'
-      }
-    }
+    response = { error: 'Server error' }
   } else {
     console.error(error)
-    response = {
-      message: error.message,
-      error
-    }
+    response = { error: error.message, object: error }
   }
   res.status(500).json(response)
 })
+
 module.exports = app
