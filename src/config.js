1d3fcdc7e2f819f053d852b85d23faf1e2c23f7c
Travis Simpkins <t73designs@yahoo.com>
2019-12-06 11:49:17 -0500

    Client Hookup

--- src/config.js
+++ src/config.js
@@ -14,7 +14,7 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     API_TOKEN: process.env.API_TOKEN || 'token',
     API_BASE_URL: 'the-console.herokuapp.com',
-    DB_URL: process.env.DB_URL || "postgresql://consoleAdmin:73rdtitan@localhost/theConsoleDB",
+    DB_URL: process.env.DB_URL || "postgres://fyxberjbsmgram:3b5dd74bbacb752ce6970bdcc42b81e4e0594683dcdc4d8e22731a9a8e1760e4@ec2-174-129-254-217.compute-1.amazonaws.com:5432/de7a6qnferjba2",
     config,
     JWT_SECRET: process.env.JWT_SECRET || 'theConsole',
     JWT_EXPIRY: process.env.JWT_EXPIRY || '2h',
