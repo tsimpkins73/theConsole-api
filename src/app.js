0f857c722a49211cd2f74cc3bae7b993c6c7f787
tsimpkins73 <46323925+tsimpkins73@users.noreply.github.com>
2019-12-06 18:31:32 -0500

    Client Hookup

--- src/app.js
+++ src/app.js
@@ -12,7 +12,11 @@ const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
 
 app.use(morgan(morganSetting))
 app.use(helmet())
-app.use(cors())
+app.use(
+  cors({
+      origin: CLIENT_ORIGIN
+  })
+);
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   next();
