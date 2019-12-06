5336b4aefa4350779c984d8e3ef23c274209a181
Travis Simpkins <t73designs@yahoo.com>
2019-12-06 13:53:51 -0500

    Client Hookup

--- src/config.js
+++ src/config.js
@@ -16,5 +16,5 @@ module.exports = {
     config,
     JWT_SECRET: process.env.JWT_SECRET || 'theConsole',
     JWT_EXPIRY: process.env.JWT_EXPIRY || '2h',
-    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://theconsole-thankful-cassowary.now.sh/',
+    Access-Control-Allow-Origin: process.env.CLIENT_ORIGIN || 'https://theconsole-thankful-cassowary.now.sh/',
   }
