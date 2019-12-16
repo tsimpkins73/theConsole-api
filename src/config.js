d2b4eb16f1d3d9895927dc89937de3de99ca3f81
tsimpkins73 <46323925+tsimpkins73@users.noreply.github.com>
2019-12-15 22:12:33 -0500

    Client Hookup

--- src/config.js
+++ src/config.js
@@ -7,7 +7,7 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     API_TOKEN: process.env.API_TOKEN || 'token',
     API_BASE_URL: 'the-console.herokuapp.com/api',
-    DB_URL: process.env.DB_URL ,
+    DB_URL: process.env.DB_URL || "postgres://fyxberjbsmgram:3b5dd74bbacb752ce6970bdcc42b81e4e0594683dcdc4d8e22731a9a8e1760e4@ec2-174-129-254-217.compute-1.amazonaws.com:5432/de7a6qnferjba2",
     JWT_SECRET: process.env.JWT_SECRET || 'theConsole',
     JWT_EXPIRY: process.env.JWT_EXPIRY || '2h',
     CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://theconsole-thankful-cassowary.now.sh/',
