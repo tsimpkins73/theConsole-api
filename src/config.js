7a54e10b37737f7f01316ce7e0b5543f00b40dd9
Travis Simpkins <t73designs@yahoo.com>
2020-01-23 12:22:02 -0500

    Clean Up

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     API_TOKEN: process.env.API_TOKEN || '872ff00f-76a9-4257-8bc4-5b692c5bfa4b',
-    API_BASE_URL: process.env.API_BASE_URL || 'https://the-console.herokuapp.com//api',
+    API_BASE_URL: process.env.API_BASE_URL || 'https://the-console.herokuapp.com/api',
     DATABASE_URL: process.env.DATABASE_URL,
     JWT_SECRET: process.env.JWT_SECRET || 'theConsole',
     JWT_EXPIRY: process.env.JWT_EXPIRY || '2h',
